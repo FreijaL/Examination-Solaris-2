9a59e32e3752c381bf002a3dc56b99c2466ae1f5
FreijaL <110614499+FreijaL@users.noreply.github.com>
2023-02-22 09:42:59 +0100

    Still in progress

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,6 @@
 const URL = 'https://majazocom.github.io/Data/solaris.json';
 const inputBtn = document.querySelector('.input-btn');
+let planetsArray = [];
 
 
 // Hämtar data från API
@@ -17,7 +18,8 @@ async function getPlanets() {
 
     planets.map(function(planet) {
         renderPlanets(planet);
-        searchInput(planet);
+        //viewResults(planet);
+        planetsArray.push(planet.name)
         return planet;
     });
 };
@@ -54,6 +56,8 @@ function openModal(planet) {
     document.querySelector('.planet-distance').innerHTML = planet.distance;
     document.querySelector('.planet-max-temp').innerHTML = planet.temp.day;
     document.querySelector('.planet-min-temp').innerHTML = planet.temp.night;
+    document.querySelector('.planet-orbital-period').innerHTML = planet.orbitalPeriod;
+    document.querySelector('.planet-moons').innerHTML = planet.moons.map((moon) => moon).join(' | ');
 
     closeModal.addEventListener('click', () => {
         modal.style.display = 'none';
@@ -67,22 +71,46 @@ function openModal(planet) {
 };
 
 
+// Funktion som öppnas när användaren sökt på något som inte matchar
+function openWrongInputModal() {
 
+}
+
+
+// Vid klick på search-knappen körs funktionen compareInput
 inputBtn.addEventListener('click', () => {
-    console.log(searchInput());
-});
+    compareInput();
+})
 
-function searchInput(planet) {
-    let inputSearch = document.querySelector('.input-search').value;
-    let planetName = planet.name;
 
-    if (inputSearch == planetName) {
-        console.log(planetName);
+// Funktion som skapar list-item med planeterna när man söker
+// function viewResults(planet) {
+//     const resultsUl = document.querySelector('.input-search-results');
+//     let resultsLi = document.createElement('li');
+//     resultsLi.innerHTML = planet.id;
+//     resultsUl.appendChild(resultsLi);
+// };
+
+
+// Funktion som jämför input-value med planetArray + lowerCase + upperCase
+function compareInput(planet) {
+    let inputValue = document.querySelector('.input-search').value;
+    let lowerCase = planetsArray.map(planet => (planet).toLowerCase());
+    let upperCase = planetsArray.map(planet => (planet).toUpperCase());
+
+    if (planetsArray.includes(inputValue) || lowerCase.includes(inputValue) || upperCase.includes(inputValue)){
+        //viewResults(planet);
+        openModal(planetsArray);
+        console.log(inputValue); 
     } else {
-        console.log('najj');
-    }
-
-    return inputSearch;
-
+        console.log('error');
+    };
 };
-//searchInput();
+ 
+
+// Eventlyssnare som ska reagera på tangent-ner och sedan jämföra med compareInput
+// let inputField = document.querySelector('.input-search');
+
+// inputField.addEventListener('onkeydown', () => {
+//     compareInput(planet);
+// });
