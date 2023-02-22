3ad730859d170f55ece7fed224bbce4cab5bb89c
FreijaL <110614499+FreijaL@users.noreply.github.com>
2023-02-22 19:25:25 +0100

    still in progress

--- Js/script.js
+++ Js/script.js
@@ -3,6 +3,7 @@ const inputBtn = document.querySelector('.input-btn');
 let planetsArray = [];
 
 
+
 // Hämtar data från API
 async function getApi() {
     let response = await fetch(URL);
@@ -12,25 +13,27 @@ async function getApi() {
 getApi();
 
 
-// Gör om datan till enskilda planeter
+
+// Gör om datan till enskilda planeter och skicka vidare informationen till de andra funktionerna
 async function getPlanets() {
     let planets = await getApi();
 
     planets.map(function(planet) {
         renderPlanets(planet);
-        //viewResults(planet);
-        planetsArray.push(planet.name)
+        renderPlanetsLi(planet);
+        planetsArray.push(planet);
         return planet;
     });
 };
 getPlanets();
 
 
+
 // Renderar ut planeterna i UI
 function renderPlanets(planet) {
     const mainEl = document.querySelector('.planets-container');
     let planetEl = document.createElement('article');
-    planetEl.classList.add('planet', `planet-${planet.id}`);
+    planetEl.classList.add('planet', `planet-${planet.id}`, `${planet.id}`);
     mainEl.appendChild(planetEl);
 
     
@@ -40,6 +43,27 @@ function renderPlanets(planet) {
         console.log(planet)
     })
 };
+
+
+
+// Skapar även li-element till listan i sökfunktionen
+function renderPlanetsLi(planet) {
+
+    const resultsUl = document.querySelector('.input-search-results');
+    let resultsLi = document.createElement('li');
+    resultsLi.classList.add('notShowing', `${planet.id}`);
+    resultsLi.innerHTML = planet.name;
+    resultsUl.appendChild(resultsLi);
+
+
+    // Öppnar modalen när användaren klickar på sökresultatet
+    resultsLi.addEventListener('click', () => {
+        openModal(planet);
+    }) 
+};
+
+console.log(planetsArray);
+
 
 
 // Funktionen som får modalen att visas / stängas samt renderar ut planeterns information
@@ -71,46 +95,66 @@ function openModal(planet) {
 };
 
 
+
 // Funktion som öppnas när användaren sökt på något som inte matchar
 function openWrongInputModal() {
 
 }
 
 
-// Vid klick på search-knappen körs funktionen compareInput
+//Vid klick på search-knappen körs funktionen compareInput
 inputBtn.addEventListener('click', () => {
     compareInput();
 })
 
 
-// Funktion som skapar list-item med planeterna när man söker
-// function viewResults(planet) {
-//     const resultsUl = document.querySelector('.input-search-results');
-//     let resultsLi = document.createElement('li');
-//     resultsLi.innerHTML = planet.id;
-//     resultsUl.appendChild(resultsLi);
-// };
 
 
 // Funktion som jämför input-value med planetArray + lowerCase + upperCase
-function compareInput(planet) {
-    let inputValue = document.querySelector('.input-search').value;
-    let lowerCase = planetsArray.map(planet => (planet).toLowerCase());
-    let upperCase = planetsArray.map(planet => (planet).toUpperCase());
-
-    if (planetsArray.includes(inputValue) || lowerCase.includes(inputValue) || upperCase.includes(inputValue)){
-        //viewResults(planet);
-        openModal(planetsArray);
-        console.log(inputValue); 
-    } else {
-        console.log('error');
-    };
-};
+// function compareInput(planet) {
+//     let inputValue = document.querySelector('.input-search').value;
+//     let arrayToLowerCase = planetsArray.map(planet => (planet.name).toLowerCase());
+//     let arrayToUpperCase = planetsArray.map(planet => (planet.name).toUpperCase());
+//     console.log(arrayToUpperCase)
+//     if (planetsArray.includes(inputValue) || arrayToLowerCase.includes(inputValue) || arrayToUpperCase.includes(inputValue)){
+        
+//         // osäker på om de lila ska ligga här eller inte
+//         renderPlanetsLi();
+//         // resultsLi.classList.remove('notShowing');
+//         // resultsLi.classList.add('showing');
+//         console.log(inputValue); 
+//     } else {
+//         console.log('error');
+//     };
+// };
  
 
-// Eventlyssnare som ska reagera på tangent-ner och sedan jämföra med compareInput
-// let inputField = document.querySelector('.input-search');
+//Eventlyssnare som ska reagera på tangent-ner och sedan jämföra med compareInput
+let inputField = document.querySelector('.input-search');
 
-// inputField.addEventListener('onkeydown', () => {
-//     compareInput(planet);
+inputField.addEventListener('keydown', (e) => {
+    const searchInput = e.target.value;
+    const filteredPlanets = planetsArray.filter((planet) => {
+        return (
+            planet.name.toLowerCase().includes(searchInput) ||
+            planet.name.toUpperCase().includes(searchInput)
+        );
+    })
+    renderPlanetsLi(filteredPlanets);
+});
+
+
+
+// denna fungerar vid knapptryck, men ger inte rätt i consolen.
+// let searchInput = document.querySelector('.input-search');
+
+// searchInput.addEventListener('keyup', (e) => {
+//     let searchString = e.target.value;
+//     let filteredPlanets = planetsArray.filter( (planet) => {
+//         return (
+//             planet.includes(searchString)
+//         );
+//     });
+//     console.log(filteredPlanets)
+//     //renderPlanetsLi(filteredPlanets);
 // });
