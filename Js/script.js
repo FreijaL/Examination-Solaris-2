c736fa32939eadd03f710982bc204c2a122bbab9
FreijaL <110614499+FreijaL@users.noreply.github.com>
2023-02-24 23:30:39 +0100

    Finished

--- Js/script.js
+++ Js/script.js
@@ -1,17 +1,13 @@
-// --- TO DO ---
-// Få bort sökreslutatet även när man raderat inputField 
-// Rensa sökfältet om man klickar utanför listan
-// Få klick utanför modal att funka ordentligt
-// Gör en ring runt saturnus
-// Få namen att duka upp när man hovrar över planeterna
-
+//   Hej Maja! Jag vet att det inte går att klicka på alla sökresultat för att solen skymmer dem.
+//   Lyckades inte lista ut hur man löste de i tid tyvärr. Men alla sökreslutat är klickbara när 
+//   dem kommer upp där solens strålar slutar..
 
 const URL = 'https://majazocom.github.io/Data/solaris.json';
-let planetsArray = [];
 const inputField = document.querySelector('.input-search');
 const ulContainer = document.querySelector('.input-search-results');
 const arrowLeft = document.querySelector('.arrow-left');
 const arrowRight = document.querySelector('.arrow-right');
+let planetsArray = [];
 let currentPlanetIndex;
 let planetEl;
 
@@ -28,7 +24,6 @@ getApi();
 // Gör om datan till enskilda planeter och skicka vidare informationen till de andra funktionerna
 async function getPlanets() {
     let planets = await getApi();
-
     planetsArray.map(function(planet) {
         renderPlanetsUI(planet);
         return planet;
@@ -42,7 +37,7 @@ function renderPlanetsUI(planet) {
     const mainEl = document.querySelector('.planets-container');
     const planetEl = document.createElement('article');
     planetEl.classList.add('planet', `planet-${planet.id}`);
-    planetEl.innerHTML = `<h2 class="planet-name-h2">${planet.name}</h2>`
+    planetEl.innerHTML = `<h2 class="planet-name-h2 planet-h2-${planet.id}">${planet.name}</h2>`
     mainEl.appendChild(planetEl);
 
     // Vid klick på planeterna öppnas Modalen
@@ -53,6 +48,7 @@ function renderPlanetsUI(planet) {
 };
 
 
+// Funktion som får ut sökresultaten i ul som li
 function renderOutMatch(planets) {
     ulContainer.innerHTML = '';
     planets.forEach(planet => {
@@ -67,22 +63,23 @@ function renderOutMatch(planets) {
 };
 
 
-// Jämför input-fältets värde med arrayens innehåll
+// Jämför input-fältets värde med planetsArray:ens innehåll
 inputField.addEventListener('keyup', function() {
     let input = inputField.value;
     let matchedPlanet = [];
-    // gå igenom listan med planet och kollar om något name inkluderar inputen
+    // Gå igenom listan med planet och kollar om någon planet inkluderar inputen
     planetsArray.forEach(planet => {
-        // om det inkluderar inputen skall vi lägga in planeten i matchedPlanet
+        // Om det inkluderar inputen skall vi lägga in planeten i matchedPlanet
         if (planet.name.toLowerCase().includes(input.toLowerCase()) && input.length >= 1){
             matchedPlanet.push(planet);
-        }
+        } 
     });
+    // Om matchedPlanet är längre än 0, dvs array:en har ett innehåll, ska resultaten visas
     if (matchedPlanet.length > 0) {
         renderOutMatch(matchedPlanet);
     } else {
-        // inga matchningar
-        ulContainer.innerHTML = 'Inga matchningar hittades';
+        // Annars finns inga matchningar och error-meddelande visas
+        ulContainer.innerHTML = 'Inga planet hittades';
     };
 });
 
@@ -122,7 +119,7 @@ function openModal(planet) {
 };
 
 
-// EventListener på pilarna för att byta modal/planet  --- fungerar ej vid sök 
+// Funktion och eventListener på pilarna för att byta modal/planet  --- fungerar ej med liknande sökresultat
 function makeArrowWork(planet) {
     currentPlanetIndex = planetsArray.findIndex(p => p.id === planet.id);
 };
